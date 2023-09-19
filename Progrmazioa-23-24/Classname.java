e13f848eab54351a1a9cc4a4c406b7f59604840c
Honey <Sandra@HP-PROBOOK-66.uni.lan>
2023-09-19 10:02:10 +0200

    Finished introduction

---
+++ Progrmazioa-23-24/Classname.java
@@ -0,0 +1,5 @@
+public class Classname { // Choose a meaningful Classname. Save as "Classname.java"
+    public static void main(String[] args) { // Entry point of the program
+        // Your programming statements here!!!
+    }
+}
