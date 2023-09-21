8bed383a65121a58f9de2e0827872cd793c992b9
Honey <Sandra@HP-PROBOOK-66.uni.lan>
2023-09-21 13:49:26 +0200

    Java aurreratuta table arte

---
+++ Progrmazioa-23-24/1-EBAL/UD1/3-ExercisesJavaBasics/src/PrintTimeTable.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class PrintTimeTable {
+    public static void main(String[] args) {
+        // Declare variables
+        final int SIZE; // size of table to be input
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the size: ");
+        SIZE = in.nextInt();
+        in.close();
+        // Print header row
+        System.out.print(" * |");
+        for (int col = 1; col <= SIZE; ++col) {
+            System.out.printf("%4d", col);
+        }
+        System.out.println(); // End row with newline
+
+        // Print separator row
+        System.out.print("----");
+
+        for (int col = 1; col <= SIZE; ++col) {
+            System.out.printf("%4s", "----");
+        }
+
+        System.out.println(); // End row with newline
+
+        // Print body using nested-loops
+        for (int row = 1; row <= SIZE; ++row) { // outer loop
+            System.out.printf("%2d |", row); // print row header first
+            for (int col = 1; col <= SIZE; ++col) { // inner loop
+                System.out.printf("%4d", row * col);
+            }
+
+            System.out.println(); // print newline after all columns
+        }
+    }
+}
