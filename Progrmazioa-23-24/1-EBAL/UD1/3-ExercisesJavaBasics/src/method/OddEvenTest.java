a121484b44d505a28e5b04a70fecf51a5b4d089a
Honey <Sandra@HP-PROBOOK-66.uni.lan>
2023-10-06 08:55:54 +0200

    Methods

---
+++ Progrmazioa-23-24/1-EBAL/UD1/3-ExercisesJavaBasics/src/method/OddEvenTest.java
@@ -0,0 +1,17 @@
+package method;
+
+import java.util.Scanner;
+
+public class OddEvenTest {
+    public static void main(String[] args) throws Exception {
+        int num;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        num = in.nextInt();
+        System.out.println(num +" is an " + );
+
+        public static boolean isOdd(int number){
+            
+        }
+    }
+}
