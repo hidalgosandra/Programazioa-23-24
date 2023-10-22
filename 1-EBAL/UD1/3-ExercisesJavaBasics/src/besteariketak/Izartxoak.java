634ea011b0917f4c51fdbca8fe75e0dba9e60ceb
HoneyEtxean <etxea>
2023-10-22 19:25:29 +0200

    CaesarStringChar

---
+++ 1-EBAL/UD1/3-ExercisesJavaBasics/src/besteariketak/Izartxoak.java
@@ -0,0 +1,26 @@
+package besteariketak;
+
+import java.util.Scanner;
+
+public class Izartxoak {
+    public static void main(String[] args) throws Exception {
+        int zen;
+        System.out.print("Esan zenbaki bat (1-10): ");
+        Scanner in = new Scanner(System.in);
+        zen = in.nextInt();
+        in.close();
+        for(int i = 1; i <= 50; i++){
+            if (i == 1){
+                System.out.print(i);
+            }
+            else if(i == 50){
+                System.out.print(" - "+i);
+            }
+            else if (i % zen==0) {
+                System.out.print(" - *" );
+            } else {
+                System.out.print(" - "+i);
+            }
+        }
+    }
+}
