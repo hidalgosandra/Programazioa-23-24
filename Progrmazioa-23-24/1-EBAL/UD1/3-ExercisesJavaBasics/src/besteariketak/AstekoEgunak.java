702d46a9f9b21cf2529d3c1f625ff8735dfd7eb4
Honey <Sandra@HP-PROBOOK-66.uni.lan>
2023-10-23 10:41:57 +0200

    AstekoEgunakEginda

--- Progrmazioa-23-24/1-EBAL/UD1/3-ExercisesJavaBasics/src/besteariketak/AstekoEgunak.java
+++ Progrmazioa-23-24/1-EBAL/UD1/3-ExercisesJavaBasics/src/besteariketak/AstekoEgunak.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AstekoEgunak {
     public static void main(String[] args) throws Exception {
         int eguna;
-        String hiz;
+        String hitz;
         String eu[] = { "Astelehena", "Asteartea", "Asteazkena", "Osteguna", "Ostirala", "Larunbata", "Igandea" };
         String en[] = { "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday", "Sunday" };
         String es[] = { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo" };
@@ -14,10 +14,20 @@ public class AstekoEgunak {
         eguna = in.nextInt();
         if (eguna > 0 && eguna <= 7) {
             System.out.print("Aukeratu hizkuntza (EU, ES, EN): ");
-            hiz = in.next();
-            
-        }else{
+            hitz = in.next();
+            String hiz = hitz.toUpperCase();
+            if (hiz.equals("EN")) {
+                System.out.print(en[eguna - 1]);
+            } else if (hiz.equals("ES")) {
+                System.out.print(es[eguna - 1]);
+            } else if (hiz.equals("EU")) {
+                System.out.print(eu[eguna - 1]);
+            } else {
+                System.out.print("Sartu hizkuntza bat hauen artean (EU, ES, EN).");
+            }
+        } else {
             System.out.println("Sartu asteko egun bat.");
         }
+        in.close();
     }
 }
