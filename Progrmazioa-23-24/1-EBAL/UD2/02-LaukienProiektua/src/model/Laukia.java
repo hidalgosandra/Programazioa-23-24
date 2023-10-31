36328d4095be1ed16e9711bf539d76c2b5ddf16b
Honey <Sandra@HP-PROBOOK-66.uni.lan>
2023-10-31 09:54:28 +0100

    01-circle and 02-Laukia

---
+++ Progrmazioa-23-24/1-EBAL/UD2/02-LaukienProiektua/src/model/Laukia.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Laukia {
+    private int zabalera;
+    private int altuera;
+
+    public Laukia() {
+        zabalera = 5;
+        altuera = 5;
+    }
+
+    public Laukia(int zab, int alt) {
+        this.zabalera = zab;
+        this.altuera = alt;
+    }
+
+    public int getZabalera() {
+        return zabalera;
+    }
+
+    public int getAltuera() {
+        return altuera;
+    }
+
+    public int getAzalera() {
+        int azalera;
+        azalera = altuera * zabalera;
+        return azalera;
+    }
+
+    public int getPerimetroa() {
+        int perimetroa;
+        perimetroa = ((altuera * 2) + (zabalera * 2));
+        return perimetroa;
+    }
+
+    public void setZabalera(int zabaleraBerria) {
+        this.zabalera = zabaleraBerria;
+    }
+
+    public void setAltuera(int altueraBerria) {
+        this.altuera = altueraBerria;
+    }
+
+    public String toString() {
+        return String.format("Laukia[" + zabalera + "x" + altuera + "]");
+    }
+}
