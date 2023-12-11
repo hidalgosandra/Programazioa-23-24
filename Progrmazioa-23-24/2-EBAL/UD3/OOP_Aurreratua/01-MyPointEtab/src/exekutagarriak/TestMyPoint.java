c6aa25fee461a29701a7db0decb4da032deeae06
hidalgosandra <114407178+hidalgosandra@users.noreply.github.com>
2023-12-11 11:42:12 +0100

    MyPoint proyect

---
+++ Progrmazioa-23-24/2-EBAL/UD3/OOP_Aurreratua/01-MyPointEtab/src/exekutagarriak/TestMyPoint.java
@@ -0,0 +1,11 @@
+package exekutagarriak;
+
+import model.MyPoint;
+
+public class TestMyPoint {
+    public static void main(String[] args) {
+        MyPoint p0 = new MyPoint(0, 0);
+        MyPoint p1 = new MyPoint(2, 1);
+        MyPoint p2 = new MyPoint(4, 0);
+    }
+}
---
+++ Progrmazioa-23-24/2-EBAL/UD3/OOP_Aurreratua/01-MyPointEtab/src/model/MyPoint.java
@@ -0,0 +1,71 @@
+package model;
+
+public class MyPoint {
+    private int x;
+    private int y;
+
+    public MyPoint() {
+        x = 5;
+        y = 5;
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance() {
+        double d, dx, dy;
+        dx = Math.pow(this.x - 0, 2);
+        dy = Math.pow(this.y - 0, 2);
+        d = Math.sqrt(dx + dy);
+        return d;
+    }
+
+    public double distance(MyPoint another) {
+        double d, dx, dy;
+        dx = Math.pow(another.x - this.x, 2);
+        dy = Math.pow(another.y - this.y, 2);
+        d = Math.sqrt(dx + dy);
+        return d;
+    }
+
+    public double distance(int x, int y) {
+        double d, dx, dy;
+        dx = Math.pow(x - 0, 2);
+        dy = Math.pow(y - 0, 2);
+        d = Math.sqrt(dx + dy);
+        return d;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[] getXY() {
+        int[] xy = { x, y };
+        return xy;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+}
