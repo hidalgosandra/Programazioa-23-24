c6b017aa16c11b8ed649b55d755aa910b15575bf
H.Sandra <114407178+hidalgosandra@users.noreply.github.com>
2024-01-15 12:19:42 +0100

    Monstruosoa

---
+++ Progrmazioa-23-24/2-EBAL/UD3/OOP_Aurreratua/04-Monstruoak/src/tableroalehenbertsioa/exekutagarriak/MonstruosokoTableroa.java
@@ -0,0 +1,10 @@
+package tableroalehenbertsioa.exekutagarriak;
+
+class MonstruosokoTableroa {
+    public static void main(String[] args) {
+        final int ROWS = 4;
+        final int COLS = 6;
+        char[][] zeldak = new char[COLS][ROWS];
+    }
+
+}
