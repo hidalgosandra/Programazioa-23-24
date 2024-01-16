37877ff6a873050b8d1f806cd01240701967d1b8
H.Sandra <114407178+hidalgosandra@users.noreply.github.com>
2024-01-16 09:42:00 +0100

    MonstruoAskoPrograma

---
+++ Progrmazioa-23-24/2-EBAL/UD3/OOP_Aurreratua/04-Monstruoak/src/herentziamemberabstraktoekin/exekutagarriak/MonstruoAskokoPrograma.java
@@ -0,0 +1,63 @@
+package herentziamemberabstraktoekin.exekutagarriak;
+
+import herentziamemberabstraktoekin.model.*;
+
+public class MonstruoAskokoPrograma {
+    private static Monster[] mk;
+    static int monsterKop = 0;
+
+    public static void main(String[] args) {
+        mk = new Monster[50];
+        monstruoakSortu(1, 2, 3);
+        monstruakBistaratu();
+        monstruoakSortu("water", 2);
+        monstruoakSortu("fire", 1);
+        monstruoakSortu("stone", 3);
+        monstruakBistaratu();
+    }
+
+    public static void monstruoakSortu(int fireKop, int waterKop, int stoneKop) {
+        for (int i = 0; i < fireKop; i++) {
+            mk[monsterKop++] = new FireMonster("F" + monsterKop);
+        }
+        for (int i = 0; i < waterKop; i++) {
+            mk[monsterKop++] = new WaterMonster("W" + monsterKop);
+        }
+        for (int i = 0; i < stoneKop; i++) {
+            mk[monsterKop++] = new StoneMonster("S" + monsterKop);
+        }
+    }
+
+    public static void monstruoakSortu(String mota, int kop) {
+        for (int i = 0; i < kop; i++) {
+            switch (mota) {
+                case "fire":
+                    mk[monsterKop] = new FireMonster("F" + (monsterKop + 1));
+                    monsterKop++;
+                    break;
+                case "water":
+                    mk[monsterKop] = new WaterMonster("W" + (monsterKop + 1));
+                    monsterKop++;
+                    break;
+                case "stone":
+                    mk[monsterKop] = new StoneMonster("S" + (monsterKop + 1));
+                    monsterKop++;
+                    break;
+                default:
+                    System.out.println("This is not a valid monter type!");
+                    break;
+            }
+        }
+    }
+
+    public static void monstruakBistaratu() {
+        int i = 0;
+        for (Monster m : mk) {
+            i++;
+            if (m != null) {
+                System.out.println(i + "- " + m.toString());
+            }
+        }
+        System.out.println("\n");
+    }
+}
--- Progrmazioa-23-24/2-EBAL/UD3/OOP_Aurreratua/04-Monstruoak/src/herentziamemberabstraktoekin/model/Monster.java
+++ Progrmazioa-23-24/2-EBAL/UD3/OOP_Aurreratua/04-Monstruoak/src/herentziamemberabstraktoekin/model/Monster.java
@@ -15,4 +15,24 @@ public abstract class Monster {
 
     /** Defines a common behavior called attack() for all its subclasses */
     public abstract String attack();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        String mota = "";
+        if (this instanceof FireMonster) {
+            mota = "FireMonster";
+        } else if (this instanceof WaterMonster) {
+            mota = "WaterMonster";
+        } else if (this instanceof StoneMonster) {
+            mota = "StoneMonster";
+        }
+        return mota + "[" + name + "]";
+    }
 }
