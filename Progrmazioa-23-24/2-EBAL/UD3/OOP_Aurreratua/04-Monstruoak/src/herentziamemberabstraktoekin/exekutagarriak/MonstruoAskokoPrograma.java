0105a12eb74de67e08dfbb3f26747bf0b2447cc6
H.Sandra <114407178+hidalgosandra@users.noreply.github.com>
2024-01-16 11:40:44 +0100

    MonsterEqualsRawrete

--- Progrmazioa-23-24/2-EBAL/UD3/OOP_Aurreratua/04-Monstruoak/src/herentziamemberabstraktoekin/exekutagarriak/MonstruoAskokoPrograma.java
+++ Progrmazioa-23-24/2-EBAL/UD3/OOP_Aurreratua/04-Monstruoak/src/herentziamemberabstraktoekin/exekutagarriak/MonstruoAskokoPrograma.java
@@ -1,12 +1,16 @@
 package herentziamemberabstraktoekin.exekutagarriak;
 
+import java.util.ArrayList;
+
 import herentziamemberabstraktoekin.model.*;
 
 public class MonstruoAskokoPrograma {
     private static Monster[] mk;
     static int monsterKop = 0;
+    static ArrayList<Monster> monstruoak = new ArrayList<Monster>();
 
     public static void main(String[] args) {
+        
         mk = new Monster[50];
         monstruoakSortu(1, 2, 3);
         monstruakBistaratu();
@@ -18,13 +22,13 @@ public class MonstruoAskokoPrograma {
 
     public static void monstruoakSortu(int fireKop, int waterKop, int stoneKop) {
         for (int i = 0; i < fireKop; i++) {
-            mk[monsterKop++] = new FireMonster("F" + monsterKop);
+            mk[monsterKop++] = new FireMonster("F" + (i+1));
         }
         for (int i = 0; i < waterKop; i++) {
-            mk[monsterKop++] = new WaterMonster("W" + monsterKop);
+            mk[monsterKop++] = new WaterMonster("W" + (i+1));
         }
         for (int i = 0; i < stoneKop; i++) {
-            mk[monsterKop++] = new StoneMonster("S" + monsterKop);
+            mk[monsterKop++] = new StoneMonster("S" + (i+1));
         }
     }
 
--- Progrmazioa-23-24/2-EBAL/UD3/OOP_Aurreratua/04-Monstruoak/src/herentziamemberabstraktoekin/model/Monster.java
+++ Progrmazioa-23-24/2-EBAL/UD3/OOP_Aurreratua/04-Monstruoak/src/herentziamemberabstraktoekin/model/Monster.java
@@ -35,4 +35,20 @@ public abstract class Monster {
         }
         return mota + "[" + name + "]";
     }
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Monster other = (Monster) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
 }
