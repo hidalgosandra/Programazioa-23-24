c01dd02dc934cf1b2cfee00cfb2bad21e29331ca
hidalgosandra <114407178+hidalgosandra@users.noreply.github.com>
2024-02-02 09:37:00 +0100

    JavaFX Button Euro Gehiago

--- Progrmazioa-23-24/2-EBAL/UD4/lehen-aplikazioa/src/main/java/dambat/SecondaryController.java
+++ Progrmazioa-23-24/2-EBAL/UD4/lehen-aplikazioa/src/main/java/dambat/SecondaryController.java
@@ -2,11 +2,26 @@ package dambat;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class SecondaryController {
+    @FXML
+    private Label zenbatEuro;
+    private int euro = 1;
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
+    }
+
+    @FXML
+    private void zenbakiaHanditu() throws IOException {
+        zenbatEuro.setText("Agur");
+    }
+
+    @FXML
+    private void gehiagoButton() throws IOException {
+        euro++;
+        zenbatEuro.setText("Zenbat euro nahi dituzu? " + euro);
     }
 }
