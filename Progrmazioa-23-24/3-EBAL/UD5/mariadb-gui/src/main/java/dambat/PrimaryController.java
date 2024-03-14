477e358e4d08b506dfbec45bbee4caab02317ad3
hidalgosandra <114407178+hidalgosandra@users.noreply.github.com>
2024-03-14 13:24:10 +0100

    02-mariadb

---
+++ Progrmazioa-23-24/3-EBAL/UD5/mariadb-gui/src/main/java/dambat/PrimaryController.java
@@ -0,0 +1,25 @@
+package dambat;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+
+    private void connect(){
+
+    }
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+    @FXML
+    private void sel(){
+
+    }
+    @FXML
+    private void SelCount(){
+
+    }
+}
