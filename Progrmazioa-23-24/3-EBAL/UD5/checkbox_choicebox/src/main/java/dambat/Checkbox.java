b89095b381bfaacc109e31ee5cb9339d3c5a0602
H.Sandra <114407178+hidalgosandra@users.noreply.github.com>
2024-03-20 17:22:01 +0100

    Checkbox funcional

--- Progrmazioa-23-24/3-EBAL/UD5/checkbox_choicebox/src/main/java/dambat/Checkbox.java
+++ Progrmazioa-23-24/3-EBAL/UD5/checkbox_choicebox/src/main/java/dambat/Checkbox.java
@@ -1,15 +1,33 @@
 package dambat;
 
 import java.io.IOException;
+import java.util.List;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Checkbox {
 
     @FXML
+    private VBox checkboxContainer;
+
+    @FXML
     private void SwitchHasierakoa() throws IOException {
         App.setRoot("Hasierakoa");
+    }
+
+    @FXML
+    private void initialize() {
+        Herriak instance = new Herriak();
+        List<String> datos = instance.irakurriItzuliDatuak();
+        for (String dato : datos) {
+            CheckBox ncheckbox = new CheckBox(dato);
+            ncheckbox.setStyle("-fx-margin: 10;");
+            checkboxContainer.getChildren().add(ncheckbox);
+        }
     }
 
 }
